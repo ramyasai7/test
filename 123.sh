3bdf253500c672bedc2089d3f90ec72fe4172b12
ramya <ramyachowdary442@gmail.cm>
2022-11-13 20:51:46 +0530

    script

---
+++ 123.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+Name="ramya"
+studentno=123
+class=10
+echo "--------
+student details--
+student name: $Name
+student number: $studentno
+student class: $class
+--------------"
+echo "---------
+  thank you
+-----------"
